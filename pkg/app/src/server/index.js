62902674f818253ff0c209d2b03d77b06690ab16
Chiel Kunkels <chiel@kunkels.me>
2018-01-02 23:51:07 +0100

    app: render body into full html document

--- pkg/app/src/server/index.js
+++ pkg/app/src/server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 
+import renderDocument from './utils/renderDocument';
+
 const app = express();
 app.disable('x-powered-by');
 
 app.get('/', (req, res) => {
-	res.send('Hello world.');
+	const body = renderDocument('Hello world.');
+	res.send(body);
 });
 
 const port = process.env.PORT || 12951;

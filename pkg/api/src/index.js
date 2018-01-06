97f19658af8fa12c7a7bf0dd213c964b7da95fc4
Chiel Kunkels <chiel@kunkels.me>
2018-01-06 22:23:05 +0100

    api: add endpoint to handle logins

--- pkg/api/src/index.js
+++ pkg/api/src/index.js
@@ -1,10 +1,22 @@
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import express from 'express';
+import uuid from 'uuid/v4';
 
 import handleErrorMiddleware from './utils/handleErrorMiddleware';
 import notFoundMiddleware from './utils/notFoundMiddleware';
 
 const app = express();
 app.disable('x-powered-by');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/login', (req, res) => {
+	const id = uuid();
+	const { username } = req.body;
+	res.json({ id, username });
+});
 
 app.use(notFoundMiddleware);
 app.use(handleErrorMiddleware);

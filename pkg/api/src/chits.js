23d5fdc39418905770a1acd5fbbb55d296be5bcd
Chiel Kunkels <chiel@kunkels.me>
2018-01-17 21:00:56 +0100

    api: add terrain + chit data

---
+++ pkg/api/src/chits.js
@@ -0,0 +1,92 @@
+export default {
+	A: {
+		letter: 'A',
+		number: 5,
+		pips: 4,
+	},
+	B: {
+		letter: 'B',
+		number: 2,
+		pips: 1,
+	},
+	C: {
+		letter: 'C',
+		number: 6,
+		pips: 5,
+	},
+	D: {
+		letter: 'D',
+		number: 3,
+		pips: 2,
+	},
+	E: {
+		letter: 'E',
+		number: 8,
+		pips: 5,
+	},
+	F: {
+		letter: 'F',
+		number: 10,
+		pips: 3,
+	},
+	G: {
+		letter: 'G',
+		number: 9,
+		pips: 4,
+	},
+	H: {
+		letter: 'H',
+		number: 12,
+		pips: 1,
+	},
+	I: {
+		letter: 'I',
+		number: 11,
+		pips: 2,
+	},
+	J: {
+		letter: 'J',
+		number: 4,
+		pips: 3,
+	},
+	K: {
+		letter: 'K',
+		number: 8,
+		pips: 5,
+	},
+	L: {
+		letter: 'L',
+		number: 10,
+		pips: 3,
+	},
+	M: {
+		letter: 'M',
+		number: 9,
+		pips: 4,
+	},
+	N: {
+		letter: 'N',
+		number: 4,
+		pips: 3,
+	},
+	O: {
+		letter: 'O',
+		number: 5,
+		pips: 4,
+	},
+	P: {
+		letter: 'P',
+		number: 6,
+		pips: 5,
+	},
+	Q: {
+		letter: 'Q',
+		number: 3,
+		pips: 2,
+	},
+	R: {
+		letter: 'R',
+		number: 11,
+		pips: 2,
+	},
+};
---
+++ pkg/api/src/terrain.js
@@ -0,0 +1,26 @@
+export default {
+	desert: {
+		name: 'Desert',
+		resources: [],
+	},
+	fields: {
+		name: 'Fields',
+		resources: ['grain'],
+	},
+	forest: {
+		name: 'Forest',
+		resources: ['lumber'],
+	},
+	hills: {
+		name: 'Hills',
+		resources: ['brick'],
+	},
+	mountains: {
+		name: 'Mountains',
+		resources: ['ore'],
+	},
+	pasture: {
+		name: 'Pasture',
+		resources: ['wool'],
+	},
+};
